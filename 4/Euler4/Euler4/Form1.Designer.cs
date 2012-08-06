e0b2feb68285f51cdd8fb5834e664f51a075838b
Colin Williams <colin.t.williams@gmail.com>
2012-08-06 18:02:57 -0500

    Euler problem #4

---
+++ 4/Euler4/Euler4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Euler4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.numInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(91, 81);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(91, 111);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 1;
+            // 
+            // numInput
+            // 
+            this.numInput.Location = new System.Drawing.Point(91, 55);
+            this.numInput.Maximum = new decimal(new int[] {
+            1661992959,
+            1808227885,
+            5,
+            0});
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(75, 20);
+            this.numInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.NumericUpDown numInput;
+    }
+}
+
---
+++ 4/Euler4/Euler4/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Euler4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int upper = 999;
+            int lower = 100;
+            int product = 1;
+
+            while (upper > lower)
+            {
+                for (int i = upper; i >= lower; i--)
+                {
+                    int testProduct = upper*i;
+                    if (testProduct < product) break;
+                    else if (isPalindrome(testProduct))
+                    {
+                        if (testProduct > product)
+                        {
+                            product = testProduct;
+                            lower = i;
+                        }
+                        else break;
+                    }
+                }
+                upper--;
+            }
+
+            lblAnswer.Text = product.ToString();
+        }
+
+        private bool isPalindrome(int number)
+        {
+            string numString = number.ToString();
+            int index = 0;
+            do
+            {
+                if (numString[index] != numString[numString.Length - 1 - index])
+                {
+                    return false;
+                }
+                index++;
+
+            } while (index < Math.Ceiling((double)numString.Length / 2));
+            return true;
+        }
+    }
+}
